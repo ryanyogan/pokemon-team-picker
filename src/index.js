366af057940ffa0358c387af7403b9cc47ca9a29
Ryan Yogan <ryanyogan@gmail.com>
2018-05-28 18:21:36 -0700

    Moved the temporary (TOY) GraphQL server URI to constants, removed the remaining React 101 stubbed data

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import registerServiceWorker from './registerServiceWorker';
 
+import { TEMP_URI } from './constants';
+
 import App from './App';
 import './index.css';
 
-const tempUri = 'https://graphql-pokemon.now.sh/graphql';
-
 const link = new HttpLink({
-  uri: tempUri,
+  uri: TEMP_URI,
 });
 
 const client = new ApolloClient({
