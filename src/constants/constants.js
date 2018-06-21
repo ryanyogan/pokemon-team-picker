e996ea9feedd3fbfd321c06ac60b6dec8674b652
Ryan Yogan <ryanyogan@gmail.com>
2018-06-20 18:36:01 -0700

    switched off the local db

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,2 @@
-// export const TEMP_URI = 'https://pokemon-server.ryanyogan.com/graphql';
-export const TEMP_URI = 'http://localhost:5000/graphql';
+export const TEMP_URI = 'https://pokemon-server.ryanyogan.com/graphql';
 export const AUTH_TOKEN = '@pokemon/token';
